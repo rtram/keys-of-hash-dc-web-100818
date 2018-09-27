5d195e8c4584f7b58e98738d09e8013382de7ece
Robin Tram <robin.tram@gmail.com>
2018-09-27 18:48:41 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
+require "pry"
+
 class Hash
   def keys_of(arguments)
-    # code goes here
+    array = []
+    arguments = arguments.to_s
+    end 
+    self.each do |key, value|
+      value = value.to_s
+      if arguments.include? value
+        array << key
+      end 
+    end 
+    array 
   end
 end
